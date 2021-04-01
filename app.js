0c7891d6ad123c5fa719102af22a1c8f9427b430
janxavierv <37735268+janxavierv@users.noreply.github.com>
2021-04-02 00:22:21 +1100

    Add images and basic information to my portfolio

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
+// Require JSON data with ES6 Destructuring
+let { data } = require("./data/data.json")
+let { projects } = data
 const express = require("express")
 const app = express();
+//Express body parser
+app.use(express.urlencoded({ extended: false }))
 app.use("/static", express.static("public"))
 app.set("view engine", "pug")
 
 
 
 app.get("/", (request, response) => {
-    response.render("index")
+    response.locals.projects = projects
+    response.render("index", projects)
+})
+
+app.get("/about", (request, response) => {
+
+    response.render("about")
 })
 app.listen(3000, () => { console.log("App.js is running in localhost:3000") })
